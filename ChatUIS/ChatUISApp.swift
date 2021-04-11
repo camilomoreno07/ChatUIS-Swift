01adb551f6c44fc83b92c6ef8ff12d140bd0a987
camilomoreno07 <morenopintocamilo07@gmail.com>
2021-04-11 12:16:02 -0500

    Initial Commit

---
+++ ChatUIS/ChatUISApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUISApp.swift
+//  ChatUIS
+//
+//  Created by Camilo Moreno on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatUISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatUIS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatUIS
+//
+//  Created by Camilo Moreno on 11/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
