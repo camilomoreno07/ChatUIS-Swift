61916f606d4bc74784079fb599feb0a380b45473
Camilo Moreno <cpinto.dev.me@gmail.com>
2021-05-17 13:00:24 -0500

    Chatview design

--- ChatUIS/ChatView.swift
+++ ChatUIS/ChatView.swift
@@ -6,41 +6,65 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ChatView: View {
     @State var mensaje = ""
     @ObservedObject private var viewModel = MsgViewModel()
     @State var scrolled = false
+    @State var email = ""
+    @State var pageNumber = 0
+
     var body: some View {
 
-        
-
+        if pageNumber == 0 {
         VStack {
-//            ScrollViewReader{reader in
-//                ScrollView{
-//                    VStack(spacing: 15){
-//                        ForEach(viewModel.msgModel){msgModel in
-//                            ChatRow(chatData: msgModel)
-//                                .onAppear{
-//                                    if msgModel == self.viewModel.msgModel.last!.id && !scrolled{
-//                                        reader.scrollTo(viewModel.msgModel.last!.id,anchor: .bottom)
-//                                        scrolled = true
-//                                    }
-//                                }
-//                        }.onChange(of: viewModel.msgModel, perform: { value in
-//                            reader.scrollTo(viewModel.msgModel.last!.id, anchor: .bottom)
-//                        })
-//                    }
-//                }
-//
-//            }
-        
             ZStack{
+                Color("verde").frame(height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                HStack{
+                    Spacer()
+                    Button(action: {
+                    let firebaseAuth = Auth.auth()
+                    do {
+                      try firebaseAuth.signOut()
+                        print("El usuario salió")
+                    } catch let signOutError as NSError {
+                      print ("Error signing out: %@", signOutError)
+                    }
+                        pageNumber = 1
+                                          }, label: {
+                        Text("Cerrar Sesión").foregroundColor(.white)
+                    }).padding(.horizontal)
+                }.frame(height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 
-                TextField("Escrbie algo", text: $mensaje)
+            }
+            List {
+                
+                
+            }.padding(0)
+            ZStack{
+                Color("verde").frame( height: 50, alignment: .bottom)
+                HStack{
+                    TextField("", text: $email)
+                        .font(.system(size: 18))
+                        .padding(.horizontal, 5)
+                        .padding(.vertical, 3)
+                        .background(RoundedRectangle(cornerRadius: 50).foregroundColor(Color.init("blanconegro")))
+                    Button(action: {
+                        print("enviar")
+                    }, label: {
+                        Image(systemName: "paperplane.fill").resizable().frame(width: 25, height: 25, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).foregroundColor(.white).padding(10)
+                    })
+                    
+                }.frame(height: 40, alignment: .center).padding(.horizontal,10)
+                
+        
             }
         }
-           
+        }
+        else{
+            FirstView()
+        }
 
     }
 }
