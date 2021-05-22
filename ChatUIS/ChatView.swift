192435bf2c702c76aa52d7ff0647eeeee5fcb111
Camilo Moreno <cpinto.dev.me@gmail.com>
2021-05-22 12:55:52 -0500

    another way to resizable keyboard

--- ChatUIS/ChatView.swift
+++ ChatUIS/ChatView.swift
@@ -14,83 +14,83 @@ struct ChatView: View {
     @State var scrolled = false
     @State var msj = ""
     @State var pageNumber = 0
-
+    
     var body: some View {
-
+        
         if pageNumber == 0 {
             VStack(spacing:0){
-            ZStack{
-                Color("verde").frame(height:50, alignment: .bottom)
-                
-                
+                ZStack{
+                    Color("verde").frame(height:50, alignment: .bottom)
+                    
+                    
                     HStack{
                         Spacer()
                         Button(action: {
-                        let firebaseAuth = Auth.auth()
-                        do {
-                          try firebaseAuth.signOut()
-                            print("El usuario salió")
-                        } catch let signOutError as NSError {
-                          print ("Error signing out: %@", signOutError)
-                        }
+                            let firebaseAuth = Auth.auth()
+                            do {
+                                try firebaseAuth.signOut()
+                                print("El usuario salió")
+                            } catch let signOutError as NSError {
+                                print ("Error signing out: %@", signOutError)
+                            }
                             pageNumber = 1
-                                              }, label: {
+                        }, label: {
                             Text("Cerrar Sesión").foregroundColor(.white)
                         }).padding(.horizontal)
                     }.frame(height: 40, alignment: .bottom)
+                    
+                    
+                }
                 
-                
-            }
-//            List(viewModel.msgModel){msgmodel in
-//                ChatRow(chatData: msgmodel)
-////                VStack{
-////                    Text(msgmodel.msg)
-////                    Text(msgmodel.user)
-////                }
-//
-//
-//
-//            }
-                ScrollView{
-                    LazyVStack {
-                        ForEach(viewModel.msgModel) { msgmodel in
-                            ChatRow(chatData: msgmodel)
+                VStack{
+                    ScrollView{
+                        LazyVStack {
+                            ForEach(viewModel.msgModel) { msgmodel in
+                                ChatRow(chatData: msgmodel)
+                            }
                         }
                     }
-                }
-            .onAppear(){
-                self.viewModel.fetchData()
-            }
-            ZStack{
-                Color("verde").frame( height: 50, alignment: .bottom)
-                HStack{
-                    TextField("", text: $msj)
-                        .font(.system(size: 18))
-                        .padding(.horizontal, 5)
-                        .padding(.vertical, 3)
-                        .background(RoundedRectangle(cornerRadius: 50).foregroundColor(Color.init("blanconegro")))
-                    if msj != ""{
-                        Button(action: {
-                            print("enviar")
-                        }, label: {
-                            Image(systemName: "paperplane.fill").resizable().frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).foregroundColor(.white).padding(10).background(Color.init("cuenta")).clipShape(Circle())
-                        })
+                    .onTapGesture {
+                        self.endEditing()
                     }
-                    
-                }.frame(height: 40, alignment: .center).padding(.horizontal,10)
+                    .onAppear(){
+                        self.viewModel.fetchData()
+                    }
+                    ZStack{
+                        Color("verde").frame( height: 50, alignment: .bottom)
+                        HStack{
+                            TextField("", text: $msj)
+                                .font(.system(size: 18))
+                                .padding(.horizontal, 5)
+                                .background(RoundedRectangle(cornerRadius: 50).foregroundColor(Color.init("blanconegro")))
+                            if msj != ""{
+                                Button(action: {
+                                    print("enviar")
+                                }, label: {
+                                    Image(systemName: "paperplane.fill").resizable().frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).foregroundColor(.white).padding(10).background(Color.init("cuenta")).clipShape(Circle())
+                                })
+                            }
+                            
+                        }.frame(height: 40, alignment: .center).padding(.horizontal,10)
+                        
+                        
+                    }
+                }.keyboardManagment()
+               
                 
-        
-            }
+                
             }.edgesIgnoringSafeArea(.all)
+            
         }
         else{
             FirstView()
         }
-
+        
+    }
+    private func endEditing() {
+        UIApplication.shared.endEditing()
     }
 }
-
-
 
 
 struct ChatView_Previews: PreviewProvider {
